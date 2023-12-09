c07e6150451807a3cf9c5529db3ba30a02b0c837
holajire03 <147520832+holajire03@users.noreply.github.com>
2023-12-09 19:59:01 +0000

    Update previous-order-page.php

--- previous-order-page.php
+++ previous-order-page.php
@@ -1,1 +1,18 @@
+<?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $product = isset($_POST['product']) ? $_POST['product'] : '';
+    $review = isset($_POST['review']) ? $_POST['review'] : '';
+
+    // Validate inputs
+    if ($product !== '' && $review !== '') {
+        // Process and store the review 
+        $response = ['success' => true, 'message' => 'Review submitted successfully']
+        echo json_encode($response);
+        exit;
+    }
+}
+
+$response = ['success' => false, 'message' => 'Invalid request'];
+echo json_encode($response);
+?>
