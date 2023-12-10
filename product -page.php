e467daca34850908efb32df576c0bd88c8982605
FED3ROUTSE <onoutse@gmail.com>
2023-12-10 15:34:15 +0000

    update products
    implemented some filters as well as their functionality

--- product -page.php
+++ product -page.php
@@ -1,12 +1,19 @@
 <?php
+require_once('connect.php');
 
-require_once ('connect.php');
+if (isset($_GET['search'])) {
+    $search = mysqli_real_escape_string($conn, $_GET['search']);
 
-$sql = "SELECT product_name, price, img FROM products";
-$result = $conn->query($sql);
+    // Modify the SQL query to include a WHERE clause for searching
+    $sql = "SELECT product_name, price, img, color, brand FROM products WHERE product_name LIKE '%$search%'";
+} else {
+    // Default query without search filter
+    $sql = "SELECT product_name, price, img, color, brand FROM products";
+}
+
+$result_products = $conn->query($sql); // Store the result in a different variable
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,143 +21,175 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Page - Shoe Emporium</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
+
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
+       body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+    font-size: 16px;
+}
 
-        header {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 1em 0;
-        }
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1em 0;
+}
 
-        nav {
-            display: flex;
-            justify-content: flex-start; /* Align headers to the left */
-            background-color: #333;
-            padding: 1em 0;
-        }
+nav {
+    display: flex;
+    justify-content: flex-start;
+    background-color: #333;
+    padding: 1em 0;
+}
 
-        nav a {
-            color: #fff;
-            text-decoration: none;
-            font-weight: bold;
-            font-size: 16px;
-            margin-right: 20px; /* Adjust the right margin as needed */
-        }
+nav a {
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 16px;
+    margin-right: 20px;
+}
 
-        #top-right {
-            position: absolute;
-            top: 10px;
-            right: 10px;
-            display: flex;
-            align-items: center;
-        }
+#top-right {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: flex;
+    align-items: center;
+}
 
-        #basket-icon,
-        #user-account {
-            margin-left: 10px;
-        }
+#basket-icon,
+#user-account {
+    margin-left: 10px;
+}
 
-        #search-container {
-            max-width: 600px;
-            margin: 20px auto;
-            text-align: center;
-        }
+/* Add a media query for smaller screens */
+@media only screen and (max-width: 768px) {
+    #top-right {
+        flex-direction: column; /* Stack items vertically */
+        align-items: flex-end; /* Align items to the right */
+    }
 
-        #search-bar {
-            width: 80%;
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            font-size: 16px;
-        }
+    #basket-icon,
+    #user-account {
+        margin-left: 0; /* Remove margin to avoid escaping the page */
+        margin-top: 5px; /* Add some top margin for separation */
+    }
+}
 
-        #search-btn {
-            padding: 10px;
-            background-color: #4CAF50;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 16px;
-        }
+#search-container {
+    max-width: 600px;
+    margin: 20px auto;
+    text-align: center;
+}
 
-        section {
-            max-width: 1200px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
+#search-bar {
+    width: 80%;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+}
 
-        #product-container {
-            display: flex;
-            overflow: hidden;
-            justify-content: space-around;
-            flex-wrap: wrap;
-            height: 22vh; /* Adjust the height as needed */
-        }
+#search-btn {
+    padding: 10px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+}
 
-        .product {
-            width: 200px;
-            height: 250px; /* Adjusted height for more content */
-            margin: 10px;
-            padding: 10px;
-            border: 1px solid #000; /* Black border */
-            text-align: center;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between; /* Added space between content */
-            background-color: #000; /* Black background */
-            color: #fff; /* White text color */
-        }
+section {
+    width: 100%; /* Full width */
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
 
-        .product img {
-            max-width: 100%;
-            max-height: 60%; /* Adjusted height for the image */
-            margin-bottom: 10px; /* Added margin below the image */
-        }
+#product-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    min-height: 300px;
+    margin: 0 auto;
+    max-width: 1200px;
+}
 
-        .product h3,
-        .product p {
-            margin: 0;
-        }
+.product {
+    width: calc(33.33% - 20px);
+    height: 300px;
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #000;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: #000;
+    color: #fff;
+    box-sizing: border-box;
+}
 
-        .product button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
+.product img {
+    max-width: 100%;
+    max-height: 50%;
+    margin-bottom: 10px;
+}
+
+.product h5,
+.product p {
+    margin: 0;
+}
+
+.product button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+
+        @media only screen and (max-width: 1200px) {
+            .product {
+                width: calc(50% - 20px);
+            }
         }
 
         @media only screen and (max-width: 768px) {
-        #product-container {
-            flex-direction: column; /* Change to a column layout on smaller screens */
-            align-items: center;
-        }
+            #product-container {
+                justify-content: center;
+            }
 
-        .product {
-            width: 80%; /* Adjust the width for better responsiveness on smaller screens */
+            .product {
+                max-width: 80%;
+            }
+
+            .product img {
+                max-height: 40%;
+            }
         }
-    }
-    </style> 
-        
+    </style>
+    
 </head>
 
 <body>
 
     <header>
-        <h1>Shoe Emporium</h1>
+    <h1>Shoe Emporium</h1>
         <nav>
             <a href="#">Home</a>
             <a href="#">Products</a>
@@ -167,38 +206,125 @@ $result = $conn->query($sql);
     </header>
 
     <div id="search-container">
-        <input type="text" id="search-bar" class="input" placeholder="Search...">
-        <button id="search-btn">Search</button>
+        <form method="GET" action="">
+            <input type="text" id="search-bar" name="search" class="input" placeholder="Search...">
+            <button type="submit" id="search-btn">Search</button>
+        </form>
     </div>
 
-    <section>
-        <h2>Featured Products</h2>
-        
-        <?php
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3">
+                <h5>Filter Products</h5>
+                <hr>
+                <h6 class="text-info">Select Brand</h6>
+                <ul class="list-group">
+                    <?php
+                    $sql_brands = "SELECT DISTINCT brand FROM products ORDER BY brand";
+                    $result_brands = $conn->query($sql_brands);
+
+                    while ($row = $result_brands->fetch_assoc()) {
+                    ?>
+                        <li class="list-group-item">
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input type="checkbox" class="form-check-input product_check" value="<?= $row['brand']; ?>" id="brand<?= $row['brand']; ?>"> <?= $row['brand']; ?>
+                                </label>
+                            </div>
+                        </li>
+                    <?php } ?>
+                </ul>
+
+                <h6 class="text-info">Select Color</h6>
+                <ul class="list-group">
+                    <?php
+                    $sql_color = "SELECT DISTINCT color FROM products ORDER BY color";
+                    $result_color = $conn->query($sql_color);
+
+                    while ($row = $result_color->fetch_assoc()) {
+                    ?>
+                        <li class="list-group-item">
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input type="checkbox" class="form-check-input color_check" value="<?= $row['color']; ?>" id="color<?= $row['color']; ?>"> <?= $row['color']; ?>
+                                </label>
+                            </div>
+                        </li>
+                    <?php } ?>
+                </ul>
+
+            </div>
+            <div class="col-lg-9">
+
+                <section>
+                    <h2>Featured Products</h2>
+                    <?php
 echo '<div id="product-container">'; // Start the container outside the loop
 
 $counter = 0;
 
-while($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result_products)) {
     // Product inside a row
-    echo '<div class="product">';
-    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['img']).'"/>';
-    echo '<h3 class="name">'.$row["product_name"].'</h3>';
-    echo '<p>'.$row["price"].'</p>';
+    echo '<div class="product" data-brand="' . $row["brand"] . '" data-color="' . $row["color"] . '">';
+    echo '<img src="data:image/jpeg;base64,' . base64_encode($row['img']) . '"/>';
+    echo '<h5 class="name">' . $row["product_name"] . '</h5>';
+    echo '<p class="details">' . $row["brand"] . ', ' . $row["color"] . '</p>';
+    echo '<p>' . $row["price"] . ' £</p>';
     echo '<button>Add to Cart</button>';
     echo '</div>'; // Close the product div
 
     $counter++;
 
-    // Check if four products are displayed, then start a new row
-    if ($counter % 4 == 0) {
+    // Check if three products are displayed, then start a new row
+    if ($counter % 3 == 0) {
         echo '</div><div id="product-container">'; // Close and reopen the container
     }
 }
 
 echo '</div>'; // Close the container after the loop
 ?>
-    </section>
+
+                </section>
+            </div>
+        </div>
+    </div>
+ 
+    <script>
+    $(document).ready(function () {
+        // Function to filter products based on selected checkboxes
+        function filterProducts() {
+            var selectedBrands = $('.product_check:checked').map(function () {
+                return $(this).val();
+            }).get();
+
+            var selectedColors = $('.color_check:checked').map(function () {
+                return $(this).val();
+            }).get();
+
+            // Hide all products
+            $('.product').hide();
+
+            // Show only products that match the selected filters
+            $('.product').each(function () {
+                var brand = $(this).data('brand');
+                var color = $(this).data('color');
+
+                if ((selectedBrands.length === 0 || selectedBrands.includes(brand)) &&
+                    (selectedColors.length === 0 || selectedColors.includes(color))) {
+                    $(this).show();
+                }
+            });
+        }
+
+        // Call the filter function on checkbox change
+        $('.product_check, .color_check').change(function () {
+            filterProducts();
+        });
+
+        // Call the filter function on page load
+        filterProducts();
+    });
+</script>
 
 </body>
 
